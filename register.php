51a63d8b14cbbe6dac0b2a85aeba4192e22aa1a8
deetirevathi <deetirevathi05@gmail.com>
2025-10-03 18:13:11 +0530

    Update register.php

--- register.php
+++ register.php
@@ -34,6 +34,7 @@ if(isset($_POST['register'])){
 <head>
 <title>Register</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="style.css" rel="stylesheet">
 </head>
 <body class="bg-light">
 <div class="container my-5 p-4 bg-white rounded shadow-sm">
